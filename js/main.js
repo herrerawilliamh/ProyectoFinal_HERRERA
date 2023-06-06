afa01e3d7c6b5d33b8e22566b4332257a2c39956
William Humberto Herrera Rey <herrerawilliamh@gmail.com>
2023-06-06 07:41:01 -0500

    Sexta subida de archivos

--- js/main.js
+++ js/main.js
@@ -10,16 +10,21 @@ const productos=[
     {id:"ropa-02", producto: "Pantalon", categoria:{nombre: "Ropa", id: "ropa"}, precio: 180000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/11450.jpg"},
     {id:"ropa-03", producto: "Chaqueta", categoria:{nombre: "Ropa", id: "ropa"}, precio: 250000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/11507.jpg"},
     {id:"ropa-04", producto: "Calcetines", categoria:{nombre: "Ropa", id: "ropa"}, precio: 50000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/3023.jpg"},
-    {id:"accesorio-01", producto: "Gorra", categoria:{nombre: "Accesorio", id: "accesorio"}, precio: 30000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/18639.jpg"},
-    {id:"accesorio-02", producto: "Bandas", categoria:{nombre: "Accesorio", id: "accesorio"}, precio: 150000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/18638.jpg"},
-    {id:"accesorio-03", producto: "Gorro", categoria:{nombre: "Accesorio", id: "accesorio"}, precio: 70000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/3021.jpg"},
-    {id:"accesorio-04", producto: "Guantes", categoria:{nombre: "Accesorio", id: "accesorio"}, precio: 40000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/3020.jpg"},
+    {id:"accesorio-01", producto: "Gorra", categoria:{nombre: "Accesorios", id: "accesorio"}, precio: 30000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/18639.jpg"},
+    {id:"accesorio-02", producto: "Bandas", categoria:{nombre: "Accesorios", id: "accesorio"}, precio: 150000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/18638.jpg"},
+    {id:"accesorio-03", producto: "Gorro", categoria:{nombre: "Accesorios", id: "accesorio"}, precio: 70000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/3021.jpg"},
+    {id:"accesorio-04", producto: "Guantes", categoria:{nombre: "Accesorios", id: "accesorio"}, precio: 40000, imagen: "https://cache.tradeinn.com/images/fotos_subfamilias/3020.jpg"},
 ];
 
-const contenedorProductos = document.querySelector("products_container");
+const contenedorProductos = document.querySelector("#products_container");
+const categoriaBotones = document.querySelectorAll(".categorie_buttom");
+const tituloPrincipal = document.querySelector("#main_title");
+let botonesAgregar = document.querySelectorAll(".product_submit");
+const number = document.querySelector("#number");
 
-function cargarProductos(){
-    productos.forEach(producto => {
+function cargarProductos(productosElegidos){
+    contenedorProductos.innerHTML = "";
+    productosElegidos.forEach(producto => {
         const div = document.createElement("div");
         div.classList.add("product");
         div.innerHTML = `
@@ -32,6 +37,53 @@ function cargarProductos(){
         `;
         contenedorProductos.append(div);
     });
+    actualizarBotonesAgregar();
 }
 
-cargarProductos();
+cargarProductos(productos);
+
+//Filtrar productos por categoría
+categoriaBotones.forEach(buttom => {
+    buttom.addEventListener("click", (e) => {
+        categoriaBotones.forEach(buttom => buttom.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+        if(e.currentTarget.id != "todos"){
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        }else{
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+    });
+})
+
+//Agregar productos al carrito
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll(".product_submit");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+const productosEnCarrito = [];
+
+function agregarAlCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    }else{
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado); 
+    }
+    actualizarNumero();
+    localStorage.setItem("productosEnCarritoCompra", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumero(){
+    let nuevoNumero = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    number.innerText = nuevoNumero;
+}
